48c29756f6a5ceab2657b1b99fc7f37684c512e7
Borys Lukianov <Borys_Lukianov@epam.com>
2017-03-20 18:48:14 +0200

    PDD Track - static resources 2

---
+++ pdd-track-web/src/main/java/com/pdd/track/conf/ThymeleafExtension.java
@@ -0,0 +1,26 @@
+package com.pdd.track.conf;
+
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
+
+import javax.annotation.PostConstruct;
+import javax.inject.Inject;
+
+@Configuration
+public class ThymeleafExtension {
+
+    @Inject
+    private SpringTemplateEngine templateEngine;
+
+    @PostConstruct
+    public void extension() {
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix("/WEB-INF/templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setCharacterEncoding("UTF-8");
+        resolver.setCacheable(false);
+        templateEngine.addTemplateResolver(resolver);
+    }
+}
