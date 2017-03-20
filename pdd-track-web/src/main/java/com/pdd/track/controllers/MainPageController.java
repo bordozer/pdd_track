98b1c8f43e35089d6081ba14c85b709246b48d3a
Borys Lukianov <Borys_Lukianov@epam.com>
2017-03-20 16:29:04 +0200

    PDD Track - Portal page

---
+++ pdd-track-web/src/main/java/com/pdd/track/controllers/MainPageController.java
@@ -0,0 +1,20 @@
+package com.pdd.track.controllers;
+
+import com.pdd.track.service.impl.DataGenerationServiceImpl;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/")
+public class MainPageController {
+
+    private static final String VIEW = "/index";
+
+    @RequestMapping(method = RequestMethod.GET, value = "/")
+    public String portalPage(final Model model) {
+        model.addAttribute("userName", DataGenerationServiceImpl.STUDENT.getName());
+        return VIEW;
+    }
+}
