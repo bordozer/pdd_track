3f5f484311fc9cec90c15166fba4d5a1059a1175
Borys Lukianov <Borys_Lukianov@epam.com>
2017-03-24 10:30:26 +0200

    PDD Track - big refactoring 3

--- pdd-track-web/src/main/java/com/pdd/track/controllers/routes/MainPageController.java
+++ pdd-track-web/src/main/java/com/pdd/track/controllers/routes/MainPageController.java
@@ -3,6 +3,7 @@ package com.pdd.track.controllers.routes;
 import com.pdd.track.service.impl.DataGenerationServiceImpl;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -12,10 +13,10 @@ public class MainPageController {
 
     private static final String VIEW = "/index";
 
-    @RequestMapping(method = RequestMethod.GET, value = "/")
-    public String portalPage(final Model model) {
+    @RequestMapping(method = RequestMethod.GET, value = "/{rulesSetKey}/")
+    public String portalPage(@PathVariable("rulesSetKey") final String rulesSetKey, final Model model) {
         model.addAttribute("studentKey", DataGenerationServiceImpl.STUDENT.getKey());
-        model.addAttribute("rulesSetKey", "1");
+        model.addAttribute("rulesSetKey", rulesSetKey);
         return VIEW;
     }
 }
